7031e511bedc522a90782a399e35ef995b6958dc
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2026-05-01 11:59:09 +0200

    Primitive datatype in typescript

---
+++ src/primitive.ts
@@ -0,0 +1,14 @@
+// JS - TS
+// string, number, boolean, ,  null, undefined
+
+// TS: never, unknown, void
+
+let userName: string = "John Doe";
+
+let userId: number = 12345;
+
+let isAdmin: boolean = true;
+isAdmin = true;
+
+let x: undefined = undefined;
+let y: null = null;

6acd5d99f193294e537c4e44083dbaf14f7f2a26
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2026-05-02 10:04:56 +0200

    Function in typescript

---
+++ src/function.ts
@@ -0,0 +1,24 @@
+// Function : arrow function and Normal function
+
+// Normal function
+function addNormal(a: number, b: number) {
+  return a + b;
+}
+
+addNormal(2, 3); // 5
+
+const addArrow = (a: number, b: number): number => {
+  return a + b;
+};
+
+// Object => function => method
+
+const user = {
+  name: "John",
+  balance: 1000,
+  addBalance(amount: number): number {
+    return this.balance + amount;
+  },
+};
+
+user.addBalance(500); // 1500

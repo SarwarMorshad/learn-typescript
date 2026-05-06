c6e67472b37afaec44e858b751a7a11c648083ab
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2026-05-06 13:10:55 +0200

    instance of typeGuard

--- src/typeGuard.ts
+++ src/typeGuard.ts
@@ -26,5 +26,60 @@ const getUserInfo = (user: NormalUser | AdminUser) => {
   }
 };
 
-getUserInfo({ name: "John" });
-getUserInfo({ name: "Jane", role: "admin" });
+// getUserInfo({ name: "John" });
+// getUserInfo({ name: "Jane", role: "admin" });
+
+// instanceof Guard
+class Person {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  getSleep(hours: number) {
+    console.log(`${this.name} is sleeping for ${hours} hours`);
+  }
+}
+
+class Student extends Person {
+  constructor(name: string) {
+    super(name);
+  }
+
+  doStudy(numberOfHours: number) {
+    console.log(`${this.name} is studying for ${numberOfHours} hours`);
+  }
+}
+
+class Teacher extends Person {
+  constructor(name: string) {
+    super(name);
+  }
+  takeClass(numberOfHours: number) {
+    console.log(`${this.name} is taking class for ${numberOfHours} hours`);
+  }
+}
+
+// function Guard
+const isStudent = (user: Person) => {
+  return user instanceof Student;
+};
+const isTeacher = (user: Person) => {
+  return user instanceof Teacher;
+};
+
+const getUserInfo1 = (user: Person) => {
+  if (isStudent(user)) {
+    user.doStudy(5);
+  } else if (isTeacher(user)) {
+    user.takeClass(3);
+  } else {
+    user.getSleep(8);
+  }
+};
+
+const student1 = new Student("Alice");
+const teacher1 = new Teacher("Bob");
+const person1 = new Person("Charlie");
+getUserInfo1(student1);
+getUserInfo1(teacher1);
+getUserInfo1(person1);
